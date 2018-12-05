3ecda945486c4e63469b167a56662593f5142fc8
Teresa Knowles <teresa07moreno@gmail.com>
2018-12-05 09:44:46 -0700

    Adds test for the joke_by_id method

--- test/user_test.rb
+++ test/user_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/user'
+require './lib/joke'
 require 'pry'
 
 class UserTest < MiniTest::Test
@@ -16,18 +17,44 @@ class UserTest < MiniTest::Test
   end
 
   def test_user_starts_with_zero_jokes
-    
+
     sal = User.new("Sal")
     assert sal.jokes.empty?
   end
 
   def test_user_can_learn_jokes
-    skip
+
     joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
     joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
     sal = User.new("Sal")
     sal.learn(joke_1)
     sal.learn(joke_2)
     assert_equal [joke_1, joke_2], sal.jokes
+  end
+
+  def test_user_can_tell_jokes_and_other_user_learns_them
+    joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
+    sal = User.new("Sal")
+    ali = User.new("Ali")
+    sal.tell(ali, joke_1)
+
+    assert_equal [joke_1], ali.jokes
+
+    sal.tell(ali, joke_2)
+    assert_equal [joke_1, joke_2], ali.jokes
+  end
+
+  def test_that_we_can_use_the_joke_id_to_return_the_joke
+    joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
+    sal = User.new("Sal")
+    ali = User.new("Ali")
+    sal.tell(ali, joke_1)
+    sal.tell(ali, joke_2)
+    assert_equal joke_1, ali.joke_by_id(1)
+
+    assert_equal joke_2, ali.joke_by_id(2)
+
   end
 end
