84791e2838cdd8671cfa5bbb2db4565654492ab9
Teresa Knowles <teresa07moreno@gmail.com>
2018-12-05 10:08:10 -0700

    Adds test for the repeated_jokes? method of OpenMic class

--- test/open_mic_test.rb
+++ test/open_mic_test.rb
@@ -33,4 +33,21 @@ class OpenMicTest < MiniTest::Test
     open_mic.welcome(ali)
     assert_equal [sal, ali], open_mic.performers
   end
+
+  def test_that_the_open_mic_knows_when_there_are_any_repeated_jokes
+    open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+    sal = User.new("Sal")
+    ali = User.new("Ali")
+    open_mic.welcome(sal)
+    open_mic.welcome(ali)
+    joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
+    ali.learn(joke_1)
+    ali.learn(joke_2)
+    refute open_mic.repeated_jokes?
+
+    ali.tell(sal, joke_1)
+    assert open_mic.repeated_jokes?
+
+  end
 end
