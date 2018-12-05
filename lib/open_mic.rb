afc6f3784957eb7299262e0e62a17e115ec94c58
Teresa Knowles <teresa07moreno@gmail.com>
2018-12-05 10:30:13 -0700

    Adds repeated_jokes? method

--- lib/open_mic.rb
+++ lib/open_mic.rb
@@ -1,8 +1,10 @@
+require 'pry'
 class OpenMic
 
   attr_reader :location,
               :date,
-              :performers
+              :performers,
+              :all_jokes
 
   def initialize(event_info)
     @location = event_info[:location]
@@ -13,5 +15,20 @@ class OpenMic
   def welcome(performer)
     @performers << performer
   end
-  
+
+  def repeated_jokes?
+    @all_jokes = []
+    @performers.each do |performer|
+      @all_jokes << performer.jokes
+    end
+    @all_jokes.flatten!
+    if @all_jokes.uniq == @all_jokes
+      false
+    elsif
+      @all_jokes.uniq != @all_jokes
+      true
+    end
+
+  end
+
 end
