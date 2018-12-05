2c4f5543f81d7cb18ff021fd277ad1114ef869b0
Teresa Knowles <teresa07moreno@gmail.com>
2018-12-05 09:58:34 -0700

    Adds welcome method to add performers

--- lib/open_mic.rb
+++ lib/open_mic.rb
@@ -1,10 +1,17 @@
 class OpenMic
 
   attr_reader :location,
-              :date
+              :date,
+              :performers
 
   def initialize(event_info)
     @location = event_info[:location]
     @date = event_info[:date]
+    @performers = []
   end
+
+  def welcome(performer)
+    @performers << performer
+  end
+  
 end
